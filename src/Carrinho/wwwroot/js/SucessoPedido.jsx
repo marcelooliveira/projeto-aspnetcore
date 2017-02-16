73e281c5375faf951e7ec536719abdb364d716a5
marcelooliveira <mclricardo@gmail.com>
2017-02-16 13:55:21 -0200

    removendo react

--- src/Carrinho/wwwroot/js/SucessoPedido.jsx
+++ src/Carrinho/wwwroot/js/SucessoPedido.jsx
@@ -6,9 +6,9 @@
         return (
             <div>
                 <br />
-                <Row>
+                <div className="row">
                     <span className="fa fa-check-circle"></span>
-                </Row>                
+                </div>                
                 <h3 className="text-center">
                     <span>Seu pedido foi enviado. Obrigado por comprar conosco!</span>
                 </h3>
@@ -22,8 +22,8 @@
                 </h3>
 
                 <Panel>
-                    <Row>
-                        <Column md={6}>
+                    <div className="row">
+                        <div className="col col-md-6">
                             <h4>
                                 <span>Nº do pedido:</span>
                                 <span className="green">{this.props.model.numeroPedido}</span>
@@ -31,74 +31,74 @@
                             <p>
                                 Você receberá um e-mail de confirmação com os detalhes do seu pedido. Por favor verifique suas configurações anti-spam do seu provedor de e-mail.
                             </p>
-                        </Column>
-                        <Column md={6}>
+                        </div>
+                        <div className="col col-md-6">
                             <h4>Forma de pagamento</h4>
                             <div className="boleto">
                                 <p><i className="fa fa-paypal leading-icon" aria-hidden="true"></i> Paypal</p>
                                 <p className="offset30"><Dollars val={this.props.model.total} /></p>
                             </div>
-                        </Column>
-                    </Row>
-                    <Row className="gray row-eq-height border-top border-bottom">
-                        <Column md={3}>
+                        </div>
+                    </div>
+                    <div className="row gray row-eq-height border-top border-bottom">
+                        <div className="col col-md-3">
                             <h4><span className="fa fa-user leading-icon"></span>Seus dados</h4>
                             <p className="offset30">{this.props.model.clienteInfo.nome}</p>
                             <p className="offset30">{this.props.model.clienteInfo.fone}</p>
-                        </Column>
-                        <Column md={3} className="border-right">
+                        </div>
+                        <div className="col col-md-3 border-right">
                             <br />
                             <br />
                             <p>{this.props.model.clienteInfo.Email}</p>
-                        </Column>
-                        <Column md={6}>
+                        </div>
+                        <div className="col col-md-6">
                             <h4><span className="fa fa-home leading-icon"></span>Endereço de entrega</h4>
                             <p className="offset30">{this.props.model.clienteInfo.enderecoEntrega}</p>
-                        </Column>
-                    </Row>
-                    <Row className="gray">
-                        <Column md={6}>
+                        </div>
+                    </div>
+                    <div className="row gray">
+                        <div className="col col-md-6">
                             <h4><span className="fa fa-gift leading-icon"></span>Entrega</h4>
-                        </Column>
-                        <Column md={6}>
+                        </div>
+                        <div className="col col-md-6">
                             <br />
                             <p className="float-right">
                                 O tempo de entrega é {this.props.model.deliveryUpTo} dias
                             </p>
-                        </Column>
-                    </Row>
-                    <Row className="gray">
-                        <Column md={6}>
+                        </div>
+                    </div>
+                    <div className="row gray">
+                        <div className="col col-md-6">
                             <p className="offset30"><b>Descrição do produto</b></p>
-                        </Column>
-                        <Column md={6} className="pull-right">
+                        </div>
+                        <div className="col col-md-6 pull-right">
                             <p><b className="float-right">Quantidade</b></p>
-                        </Column>
-                    </Row>
+                        </div>
+                    </div>
                     { this.props.model.itemsCarrinho.map(item =>
-                        <Row className="gray">
-                            <Column md={6}>
+                        <div className="row gray">
+                            <div className="col col-md-6">
                                 <div className="offset30 truncate">
                                     <span>•</span>
                                     <span>{item.descricao}</span>
                                 </div>
-                            </Column>
-                            <Column md={6} className="pull-right">
+                            </div>
+                            <div className="col col-md-6 pull-right">
                                 <p className="float-right">{item.quantidade}</p>
-                            </Column>
-                        </Row>
+                            </div>
+                        </div>
                             )
                     }
                 </Panel>
 
-                <Row>
-                    <Column md={9}></Column>
+                <div className="row">
+                    <div className="col col-md-9"></div>
                     <a href="/">
-                        <Column md={2}>
+                        <div className="col col-md-2">
                             <Button bsStyle="success">Voltar para o catálogo de produtos</Button>
-                        </Column>
+                        </div>
                     </a>
-                </Row>
+                </div>
             </div>
       );
     }
