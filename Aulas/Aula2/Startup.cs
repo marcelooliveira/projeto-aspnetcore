0acfe609c0fbdb4f45c072d264ebef18858aaac8
marcelooliveira <mclricardo@gmail.com>
2017-02-21 18:34:21 -0300

    PedidoController

--- Aulas/Aula2/Startup.cs
+++ Aulas/Aula2/Startup.cs
@@ -34,7 +34,6 @@ namespace WebApplication
             Configuration = builder.Build();
         }
 
-
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
