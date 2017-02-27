e5075c4557e1f17701c586e7c9d80080441c2d58
marcelooliveira <mclricardo@gmail.com>
2017-02-27 13:39:03 -0300

    Aula4 - início

---
+++ Aulas/Aula4/Controllers/PedidoController.cs
@@ -0,0 +1,47 @@
+﻿using Aula3.Models;
+using Aula3.Models.PedidoViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication.Controllers
+{
+    public class PedidoController : Controller
+    {
+        readonly List<Produto> produtos = new List<Produto>() {
+            new Produto(1, "produto 1", 49.90m ),
+            new Produto(2, "produto 2", 59.90m ),
+            new Produto(3, "produto 3", 39.90m ),
+            new Produto(4, "produto 4", 129.90m )
+        };
+
+        readonly List<ItemPedido> itensPedido;
+
+        public PedidoController()
+        {
+            itensPedido = new List<ItemPedido>()
+            {
+                new ItemPedido(1, produtos[0], 1, produtos[0].Preco),
+                new ItemPedido(1, produtos[1], 2, produtos[1].Preco),
+                new ItemPedido(1, produtos[2], 3, produtos[2].Preco),
+                new ItemPedido(1, produtos[3], 2, produtos[3].Preco)
+            };
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(produtos);
+        }
+
+        [ResponseCache(NoStore=true)]
+        public IActionResult Carrinho()
+        {
+            return View(new CarrinhoViewModel(itensPedido));
+        }
+
+        [ResponseCache(NoStore = true)]
+        public IActionResult Resumo()
+        {
+            return View(itensPedido);
+        }
+    }
+}
---
+++ Aulas/Aula4/Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula3.Models
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Preco { get; private set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(int id, 
+                        string descricao,
+                        decimal preco)
+        {
+            this.Id = id;
+            this.Descricao = descricao;
+            this.Preco = preco;
+        }
+    }
+}
