d40dc68806232f469eec7db63b3ccf5ffe6ce2eb
marcelooliveira <mclricardo@gmail.com>
2017-04-03 19:03:08 -0300

    aspnetcore-aula3.4-Criar o Modelo: Item de Pedido
    aspnetcore-aula3.4-Criar o Modelo: Item de Pedido

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -9,27 +9,35 @@ namespace CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
+        List<Produto> produtos = new List<Produto>
+        {
+            new Produto(1, "Sleep not found", 59.90m),
+            new Produto(2, "May the code be with you", 59.90m),
+            new Produto(3, "Rollback", 59.90m),
+            new Produto(4, "REST", 69.90m),
+            new Produto(5, "Design Patterns com Java", 69.90m),
+            new Produto(6, "Vire o jogo com Spring Framework", 69.90m),
+            new Produto(7, "Test-Driven Development", 69.90m),
+            new Produto(8, "iOS: Programe para iPhone e iPad", 69.90m),
+            new Produto(9, "Desenvolvimento de Jogos para Android", 69.90m)
+        };
+
         public IActionResult Carrossel()
         {
-            var produtos = new List<Produto>
-            {
-                new Produto(1, "Sleep not found", 59.90m),
-                new Produto(2, "May the code be with you", 59.90m),
-                new Produto(3, "Rollback", 59.90m),
-                new Produto(4, "REST", 69.90m),
-                new Produto(5, "Design Patterns com Java", 69.90m),
-                new Produto(6, "Vire o jogo com Spring Framework", 69.90m),
-                new Produto(7, "Test-Driven Development", 69.90m),
-                new Produto(8, "iOS: Programe para iPhone e iPad", 69.90m),
-                new Produto(9, "Desenvolvimento de Jogos para Android", 69.90m)
-            };
 
             return View(produtos);
         }
 
         public IActionResult Carrinho()
         {
-            return View();
+            var itensCarrinho = new List<ItemPedido>
+            {
+                new ItemPedido(1, produtos[0], 1),
+                new ItemPedido(2, produtos[1], 2),
+                new ItemPedido(3, produtos[2], 3)
+            };
+
+            return View(itensCarrinho);
         }
 
         public IActionResult Resumo()
