e8f24a477174180aadb6b4cb0ae1b514ff26445e
marcelooliveira <mclricardo@gmail.com>
2017-04-11 15:16:40 -0300

    aspnetcore-aula6.8-Alterar subtotal, total automaticamente

--- CasaDoCodigo/wwwroot/js/carrinho.js
+++ CasaDoCodigo/wwwroot/js/carrinho.js
@@ -35,16 +35,32 @@
             data: JSON.stringify(data)
         }).done(function (response) {
             this.setQuantidade(response.itemPedido);
+            this.setSubtotal(response.itemPedido);
+            this.setTotal(response.carrinhoViewModel);
         }.bind(this));
     }
 
     setQuantidade(itemPedido) {
-        $('[item-id=' + itemPedido.id + ']')
+        this.getLinhaDoItem(itemPedido)
             .find('input').val(itemPedido.quantidade);
+    }
+
+    setSubtotal(itemPedido) {
+        this.getLinhaDoItem(itemPedido)
+            .find('[subtotal]').html(itemPedido.subtotal.duasCasas());
+    }
+
+    setTotal(carrinhoViewModel) {
+        $('[total]').html(carrinhoViewModel.total.duasCasas());
+    }
+
+    getLinhaDoItem(itemPedido) {
+        return $('[item-id=' + itemPedido.id + ']');
     }
 }
 
 var carrinho = new Carrinho();
 
-
-
+Number.prototype.duasCasas = function () {
+    return this.toFixed(2).replace('.', ',');
+}
